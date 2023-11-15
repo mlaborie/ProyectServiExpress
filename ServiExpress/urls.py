24b8e8214a16d7d64d3b0393d32b0ed271c4a5c2
Francisco Egenau <fegenau@gmail.com>
2023-11-14 21:43:03 -0300

    Template index, con promocion 1

--- ServiExpress/urls.py
+++ ServiExpress/urls.py
@@ -4,7 +4,9 @@ from . import views
 urlpatterns = [
     # Otras URL de tu aplicación aquí
 
-    path('', views.home, name='home'),  # Esta línea configura la página de inicio
+    
 
-    path('home/', views.home, name='home'),    
+    path('', views.index, name='home'),  # Esta línea configura la página de inicio
+
+    path('home/', views.home, name='incio'),    
 ]
