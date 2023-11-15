3efce629262644b26e5b14f82b457ab2b575cc07
mlaborie <ma.laborie@duocuc.cl>
2023-11-15 19:54:46 -0300

    Test

--- ServiExpress/urls.py
+++ ServiExpress/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),  # Esta ruta vacía representa la página de inicio
     # Otras rutas y vistas pueden ir aquí si es necesario
+
+    
 ]
