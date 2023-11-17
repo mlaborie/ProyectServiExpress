4590b3411ab13bc0b8f549061329c26023bfcd12
mlaborie <ma.laborie@duocuc.cl>
2023-11-17 18:17:18 -0300

    Html Pack

--- ServiExpress/urls.py
+++ ServiExpress/urls.py
@@ -5,6 +5,9 @@ urlpatterns = [
     path('', views.index, name='index'),  # Esta ruta vacía representa la página de inicio
     # Otras rutas y vistas pueden ir aquí si es necesario
 
+# Gestionar Proveedores
+        path('crear_proveedor/', views.crear_proveedor, name='crear_proveedor'),
+        path('proveedores/', views.lista_proveedores, name='lista_proveedores'),
+        path('proveedores/editar/<int:proveedor_id>/', views.editar_proveedor, name='editar_proveedor'),
         path('checkout/', views.checkout, name='checkout'),
-
 ]
--- ServiExpress/views.py
+++ ServiExpress/views.py
@@ -1,9 +1,41 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from ServiExpress.models import *
+from .forms import  ProveedorForm
+from django.contrib import messages
 
 def index(request):
     # Agrega aquí la lógica que desees para tu página de inicio
     return render(request, 'index.html')  # Aquí se renderiza un archivo HTML para la página de inicio
 
-
 def checkout(request):
-    return render(request, 'checkout.html')
+    return render(request, 'checkout.html') 
+
+
+#Gestion De Proveedores
+
+def crear_proveedor(request):
+    if request.method == 'POST':
+        form = ProveedorForm(request.POST)
+        if form.is_valid():
+            form.save()  # Guarda el proveedor en la base de datos
+            messages.success(request, 'Proveedor creado con éxito.')  # Agrega un mensaje de éxito
+            return redirect('../crear_proveedor')  # Redirige a la página principal o donde desees
+    else:
+        form = ProveedorForm()
+
+    return render(request, 'ModuloGestionProveedores/crear_proveedor.html', {'form': form})
+
+def lista_proveedores(request):
+    proveedores = Proveedor.objects.all()
+    return render(request, 'ModuloGestionProveedores/lista_proveedores.html', {'proveedores': proveedores})
+
+def editar_proveedor(request, proveedor_id):
+    proveedor = Proveedor.objects.get(pk=proveedor_id)
+    if request.method == 'POST':
+        form = ProveedorForm(request.POST, instance=proveedor)
+        if form.is_valid():
+            form.save()
+            return redirect('lista_proveedores')
+    else:
+        form = ProveedorForm(instance=proveedor)
+    return render(request, 'ModuloGestionProveedores/editar_proveedor.html', {'form': form, 'proveedor': proveedor})
