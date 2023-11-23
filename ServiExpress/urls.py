7dc1a97b157405709ecccd308ed4e96da79c687b
Francisco Egenau <fegenau@gmail.com>
2023-11-23 00:57:02 -0300

    SE CREA BASE.HTML

--- ServiExpress/urls.py
+++ ServiExpress/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),  # Esta ruta vacía representa la página de inicio
+        path('', views.index, name='index'),  # Esta ruta vacía representa la página de inicio
     # Otras rutas y vistas pueden ir aquí si es necesario
 
 # Gestionar Proveedores
